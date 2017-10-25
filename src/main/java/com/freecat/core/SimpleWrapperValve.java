7c3154517d2cb95ffad81b8c1ba6c7c8ac7b5b8f
liyuan <1835630110@qq.com>
2017-10-25 23:06:14 +0800

    http相关的优化

---
+++ src/main/java/com/freecat/core/SimpleWrapperValve.java
@@ -0,0 +1,51 @@
+package com.freecat.core;
+
+import com.freecat.container.Container;
+import com.freecat.container.SimpleWrapper;
+import com.freecat.http.HttpRequest;
+import com.freecat.http.HttpResponse;
+import com.freecat.pipeline.Contained;
+import com.freecat.pipeline.Valve;
+import com.freecat.pipeline.ValveContext;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleWrapperValve implements Valve, Contained {
+
+	protected Container container;
+
+	public void invoke(HttpRequest request, HttpResponse response,
+					   ValveContext valveContext) throws IOException, ServletException {
+
+		SimpleWrapper wrapper = (SimpleWrapper) getContainer();
+		Servlet servlet = null;
+		// Allocate a servlet instance to process this request
+		try {
+			servlet = wrapper.allocate();
+			if (request != null && response != null) {
+				servlet.service(request, response);
+			} else {
+				servlet.service(request, response);
+			}
+		} catch (ServletException e) {
+		}
+	}
+
+	public String getInfo() {
+		return null;
+	}
+
+	public Container getContainer() {
+		return container;
+	}
+
+	public void setContainer(Container container) {
+		this.container = container;
+	}
+}
