e0edd182960af8bfd0dce2f887808f5176399191
liyuan <1835630110@qq.com>
2017-10-27 21:16:32 +0800

    支持map映射

--- src/main/java/com/freecat/container/Context.java
+++ src/main/java/com/freecat/container/Context.java
@@ -1,12 +1,10 @@
 package com.freecat.container;
 
-import com.freecat.util.CharsetMapper;
-
-import javax.servlet.ServletContext;
-
-/**
- * Created by liyuan on 17-10-18.
+/***
+ * context容器
  */
+
+
 public interface Context extends Container{
     void addServletMapping(String s, String primitive);
 }
